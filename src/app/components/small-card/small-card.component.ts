c0fbc14819fa13e807fbab48ac82c65fc0139c30
MaxwellSSales <maxkyluah@hotmail.com>
2025-04-06 01:51:45 -0300

    components dinamycs propertys

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   standalone: false,
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover: string =
+    'https://animefriends.com.br/wp-content/uploads/2025/03/INGRESSOS.png';
+  @Input()
+  cardTitle: string = 'Ingressos AnimeFriends 2025';
 
+  constructor() {}
 }
